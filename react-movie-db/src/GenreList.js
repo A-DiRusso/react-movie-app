0cc4b650979e185d71ad39deb8d63d5d49ef40e7
Anthony DiRusso <ossurid@gmail.com>
2019-04-25 18:18:11 -0400

    fixes several bugs

--- react-movie-db/src/GenreList.js
+++ react-movie-db/src/GenreList.js
@@ -1,11 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
-function GenreList() {
+function GenreList(props) {
   return (
-    <div>
+      <ul> 
+            {
+                props.genres.map((genre, i) => {
+                    //use explicit return to have mulitple lines use {} to do
+                    return (
+                    <li key={i}>
+                        <a 
+                            href="#"
+                            onClick={() => {
+                                
+                                props.handleClick(genre);
+                            }}
+                        >{genre}</a>
+                    </li>)
+                })
+            }
+       </ul>
       
-    </div>
-  )
+    );
 }
 
 
